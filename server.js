1b9ea269c63149743ed81ff4f58568da3607c8aa
petersorensen <phojholt@aol.com>
2016-08-16 10:18:29 +0200

    express-3 version

--- server.js
+++ server.js
@@ -15,7 +15,8 @@ var hostname = 'localhost';
 //app.use(cors());
 app.use(morgan('dev'));
 
-app.use(function(req, res, next) {
+app.all('*', function(req, res, next) {
+//app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
