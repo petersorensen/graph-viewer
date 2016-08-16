15262c2fefe266360ddf1214a3e501c5aa8dc6d2
petersorensen <phojholt@aol.com>
2016-08-16 09:56:08 +0200

    express-1 version

--- server.js
+++ server.js
@@ -4,20 +4,46 @@ var http = require("http");
 var fs = require("fs");
 var path = require("path");
 var mime = require("mime");
-//var express = require('express'),
-//    cors = require('cors');
-//  , app = express();
-/* 
-app.use(cors());
+var express = require('express'),
+    cors = require('cors'),
+    app = express();
+  var morgan = require('morgan');
  
-app.get('/products/:id', function(req, res, next){
+var port = process.env.PORT || 3000;
+var hostname = 'localhost';
+
+app.use(cors());
+app.use(morgan('dev'));
+
+/*
+http.createServer(function(request, response) {
+  response.writeHead(200, {"Content-Type": "text/plain"});
+  response.write("It's alive!");
+  response.end();
+}).listen(portnumber);
+*/
+
+app.use(express.static(__dirname+'/public'));
+
+/*app.listen(port,hostname,function(){
+	console.log("Server running at http://${hostname};${port}/");
+
+//res.writeHead(200,{'Content-Type' : 'text/html'});
+//res.end('<html><body><h1>Hello World</h1></body></html>');
+
+});
+ */
+var server = app.listen(port);
+
+//var server = http.createServer(app);
+
+/*app.get('/products/:id', function(req, res, next){
   res.json({msg: 'This is CORS-enabled for all origins!'});
 });
  
 app.listen(80, function(){
   console.log('CORS-enabled web server listening on port 80');
 });
-
 */
 
 function send404(response) {
@@ -46,7 +72,7 @@ function serverWorking(response, absPath) {
     }
   });
 }
-
+/*
 var server = http.createServer(function(request, response) {
   var filePath = false;
 
@@ -59,9 +85,9 @@ var server = http.createServer(function(request, response) {
   var absPath = "./" + filePath;
   serverWorking(response, absPath);
 });
-
+*/
 //server.use(cors());
-var port_number = server.listen(process.env.PORT || 3000);
+//var port_number = server.listen(process.env.PORT || 3000);
 /*
 http.createServer(function(request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
